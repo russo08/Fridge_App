2ae0d8fb0fef619e0ede46a1ca1387f78e153e43
Eduardo Maya <russomaya08@gmail.com>
2026-08-28 13:25:12 -0500

    create base template

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return render_template("base.html")
+
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+if __name__ in "__main__":
+    app.run(debug=True)
