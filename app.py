79997f177987d48ec07948239c250d3107ff0a69
Eduardo Maya <russomaya08@gmail.com>
2026-09-06 23:42:19 -0500

    Add new add item page

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField, IntegerField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "some password that no one knows"
@@ -26,7 +28,23 @@ def add_Item():
     days_to_exp = None
     item_location = None
     form = AddItemForm()
+
+    if form.validate_on_submit():
+        flash("Item Added Succesfully")
     return render_template("add_item.html", form=form)
+
+
+@app.route('/Edit_Item', methods=['GET', 'POST'])
+def edit_Item():
+    item_name = None
+    item_count = None
+    days_to_exp = None
+    item_location = None
+    form = AddItemForm()
+
+    if form.validate_on_submit():
+        flash("Item Edited Succesfully")
+    return render_template("edit_item.html", form=form)
 
 if __name__ in "__main__":
     app.run(debug=True)
